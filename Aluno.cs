0c013f371369a389bb1a2532d8eef95182016c3b
Gaspares1978 <41311144+Gaspares1978@users.noreply.github.com>
2018-12-06 16:42:46 -0200

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste04
+{
+    class Aluno
+    {
+        public string notas;
+        public double nota1, nota2, nota3;
+        public string nome;
+
+        public double notaFinal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+        public bool Aprovado()
+        {
+            if (notaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+            public double QuantoFalta()
+            {
+                if (Aprovado())
+                {
+                    return 0.0;
+                }
+                else
+                {
+                    return 60.0 - notaFinal();
+                }
+            }
+        }
+
+    }
+
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno x = new Aluno();
+            x.nome = Console.ReadLine();
+            x.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("NOTA FINAL = "
++ x.notaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (x.Aprovado())
+
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram" + x.QuantoFalta().ToString("F2", CultureInfo.InvariantCulture)+ "Pontos");
+            }
+            Console.ReadLine();
+        }
+    }
+}
